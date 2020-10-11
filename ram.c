bfc84caab2e55f29c978c6f3454c334bac0793d2
cheminacci <65716931+cheminacci@users.noreply.github.com>
2020-10-11 14:10:16 -0400

    Add files via upload
    Added the rest of the /proc/meminfo into into the ram.c
    
    I did this as a quick fix to make that RAM usage on my bar more accurate. I notices that the RAM in slstatus would constantly grow over time after a lot of use. With wine, or different browsers. Growing much greater than the values found in a htop, top, and /usr/bin/free. After doing some research and calculations I realized that the Sreclaimable memory was causing the the large difference in values.
    
    I modified the calculation. The ram.c files goes in the components directory of your slstatus.
    
    This is essentially a rough draft , but it works VERY well, and gives me the same values that you would get from free -m in the terminal.

---
+++ ram.c
@@ -0,0 +1,302 @@
+/* See LICENSE file for copyright and license details. */
+#include <stdio.h>
+
+#include "../util.h"
+
+#if defined(__linux__)
+	#include <stdint.h>
+
+	const char *
+	ram_free(void)
+	{
+		uintmax_t free;
+
+		if (pscanf("/proc/meminfo",
+		           "MemTotal: %ju kB\n"
+		           "MemFree: %ju kB\n"
+		           "MemAvailable: %ju kB\n",
+		           &free, &free, &free) != 3) {
+			return NULL;
+		}
+
+		return fmt_human(free * 1024, 1024);
+	}
+
+	const char *
+	ram_perc(void)
+	{
+	uintmax_t total, free, m_avail, buffers, cached;
+	uintmax_t scached,active,inactive,active_a,inactive_a,active_f,inactive_f;
+	uintmax_t unevict, mlocked, swaptotal, swapfree, dirty,writeback, anonpages;
+	uintmax_t mapped, shmem, kreclaim, slab, sreclaim;
+                                                                                                                                                                                                                                                                 
+	if (pscanf("/proc/meminfo",
+	           "MemTotal: %ju kB\n"
+	           "MemFree: %ju kB\n"
+	           "MemAvailable: %ju kB\n"
+	           "Buffers: %ju kB\n"
+	           "Cached: %ju kB\n"
+	           "SwapCached: %ju kB\n"
+	           "Active: %ju kB\n"
+	           "Inactive: %ju kB\n"
+	           "Active(anon): %ju kB\n"
+	           "Inactive(anon): %ju kB\n"
+	           "Active(file): %ju kB\n"
+	           "Inactive(file): %ju kB\n"
+	           "Unevictable: %ju kB\n"
+	           "Mlocked: %ju kB\n"
+	           "SwapTotal: %ju kB\n"
+	           "SwapFree: %ju kB\n"
+	           "Dirty: %ju kB\n"
+	           "Writeback: %ju kB\n"
+	           "AnonPages: %ju kB\n"
+	           "Mapped: %ju kB\n"
+	           "Shmem: %ju kB\n"
+	           "KReclaimable: %ju kB\n"
+	           "Slab: %ju kB\n"
+	           "SReclaimable: %ju kB\n",
+	           
+	           &total, &free, &m_avail, &buffers, &cached, &scached,&active,&inactive,&active_a,&inactive_a,&active_f,&inactive_f,&unevict,&mlocked,&swaptotal,&swapfree, &dirty, &writeback, &anonpages, &mapped, &shmem, &kreclaim, &slab, &sreclaim) !=24) {
+			return NULL;
+		}
+
+		if (total == 0) {
+			return NULL;
+		}
+
+		return bprintf("%d", 100 * ((total - free) - (buffers + cached + sreclaim))
+                               / total);
+	}
+
+	const char *
+	ram_total(void)
+	{
+		uintmax_t total;
+
+		if (pscanf("/proc/meminfo", "MemTotal: %ju kB\n", &total)
+		    != 1) {
+			return NULL;
+		}
+
+		return fmt_human(total * 1024, 1024);
+	}
+
+	const char *
+	ram_used(void)
+	{
+		uintmax_t total, free, m_avail, buffers, cached;
+		uintmax_t scached,active,inactive,active_a,inactive_a,active_f,inactive_f;
+		uintmax_t unevict, mlocked, swaptotal, swapfree, dirty,writeback, anonpages;
+		uintmax_t mapped, shmem, kreclaim, slab, sreclaim;
+
+		if (pscanf("/proc/meminfo",
+		           "MemTotal: %ju kB\n"
+		           "MemFree: %ju kB\n"
+		           "MemAvailable: %ju kB\n"
+		           "Buffers: %ju kB\n"
+		           "Cached: %ju kB\n"
+		           "SwapCached: %ju kB\n"
+		           "Active: %ju kB\n"
+		           "Inactive: %ju kB\n"
+		           "Active(anon): %ju kB\n"
+		           "Inactive(anon): %ju kB\n"
+		           "Active(file): %ju kB\n"
+		           "Inactive(file): %ju kB\n"
+		           "Unevictable: %ju kB\n"
+		           "Mlocked: %ju kB\n"
+		           "SwapTotal: %ju kB\n"
+		           "SwapFree: %ju kB\n"
+		           "Dirty: %ju kB\n"
+		           "Writeback: %ju kB\n"
+		           "AnonPages: %ju kB\n"
+		           "Mapped: %ju kB\n"
+		           "Shmem: %ju kB\n"
+		           "KReclaimable: %ju kB\n"
+		           "Slab: %ju kB\n"
+		           "SReclaimable: %ju kB\n",
+		           
+		           &total, &free, &m_avail, &buffers, &cached, &scached,&active,&inactive,&active_a,&inactive_a,&active_f,&inactive_f,&unevict,&mlocked,&swaptotal,&swapfree, &dirty, &writeback, &anonpages, &mapped, &shmem, &kreclaim, &slab, &sreclaim) !=24) {
+			return NULL;
+		}
+
+		return fmt_human((total - ( free + buffers + cached + sreclaim )) * 1024,
+		                 1024);
+
+
+/*
+		
+	const char *
+	ram_used(void)
+	{
+		uintmax_t total, free, m_avail, buffers, cached;
+
+		if (pscanf("/proc/meminfo",
+		           "MemTotal: %ju kB\n"
+		           "MemFree: %ju kB\n"
+		           "MemAvailable: %ju kB\n"
+		           "Buffers: %ju kB\n"
+		           "Cached: %ju kB\n",
+		           &total, &free, &m_avail, &buffers, &cached) != 5) {
+			return NULL;
+		}
+
+		return fmt_human((total - ( free + buffers + cached )) * 1024,
+		                 1024);
+		return fmt_human((total - free - buffers - cached) * 1024,
+		                 1024);
+
+*/
+
+
+	}
+
+
+
+
+
+
+
+#elif defined(__OpenBSD__)
+	#include <stdlib.h>
+	#include <sys/sysctl.h>
+	#include <sys/types.h>
+	#include <unistd.h>
+
+	#define LOG1024 10
+	#define pagetok(size, pageshift) (size_t)(size << (pageshift - LOG1024))
+
+	inline int
+	load_uvmexp(struct uvmexp *uvmexp)
+	{
+		int uvmexp_mib[] = {CTL_VM, VM_UVMEXP};
+		size_t size;
+
+		size = sizeof(*uvmexp);
+
+		if (sysctl(uvmexp_mib, 2, uvmexp, &size, NULL, 0) >= 0) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	const char *
+	ram_free(void)
+	{
+		struct uvmexp uvmexp;
+		int free_pages;
+
+		if (load_uvmexp(&uvmexp)) {
+			free_pages = uvmexp.npages - uvmexp.active;
+			return fmt_human(pagetok(free_pages, uvmexp.pageshift) *
+			                 1024, 1024);
+		}
+
+		return NULL;
+	}
+
+	const char *
+	ram_perc(void)
+	{
+		struct uvmexp uvmexp;
+		int percent;
+
+		if (load_uvmexp(&uvmexp)) {
+			percent = uvmexp.active * 100 / uvmexp.npages;
+			return bprintf("%d", percent);
+		}
+
+		return NULL;
+	}
+
+	const char *
+	ram_total(void)
+	{
+		struct uvmexp uvmexp;
+
+		if (load_uvmexp(&uvmexp)) {
+			return fmt_human(pagetok(uvmexp.npages,
+			                         uvmexp.pageshift) * 1024,
+			                 1024);
+		}
+
+		return NULL;
+	}
+
+	const char *
+	ram_used(void)
+	{
+		struct uvmexp uvmexp;
+
+		if (load_uvmexp(&uvmexp)) {
+			return fmt_human(pagetok(uvmexp.active,
+			                         uvmexp.pageshift) * 1024,
+			                 1024);
+		}
+
+		return NULL;
+	}
+#elif defined(__FreeBSD__)
+	#include <sys/sysctl.h>
+	#include <sys/vmmeter.h>
+	#include <unistd.h>
+	#include <vm/vm_param.h>
+
+	const char *
+	ram_free(void) {
+		struct vmtotal vm_stats;
+		int mib[] = {CTL_VM, VM_TOTAL};
+		size_t len;
+
+		len = sizeof(struct vmtotal);
+		if (sysctl(mib, 2, &vm_stats, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		return fmt_human(vm_stats.t_free * getpagesize(), 1024);
+	}
+
+	const char *
+	ram_total(void) {
+		long npages;
+		size_t len;
+
+		len = sizeof(npages);
+		if (sysctlbyname("vm.stats.vm.v_page_count", &npages, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		return fmt_human(npages * getpagesize(), 1024);
+	}
+
+	const char *
+	ram_perc(void) {
+		long npages;
+		long active;
+		size_t len;
+
+		len = sizeof(npages);
+		if (sysctlbyname("vm.stats.vm.v_page_count", &npages, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		if (sysctlbyname("vm.stats.vm.v_active_count", &active, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		return bprintf("%d", active * 100 / npages);
+	}
+
+	const char *
+	ram_used(void) {
+		long active;
+		size_t len;
+
+		len = sizeof(active);
+		if (sysctlbyname("vm.stats.vm.v_active_count", &active, &len, NULL, 0) == -1
+				|| !len)
+			return NULL;
+
+		return fmt_human(active * getpagesize(), 1024);
+	}
+#endif
